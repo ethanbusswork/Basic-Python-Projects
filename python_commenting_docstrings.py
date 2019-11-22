6389463463e723c0b098da2e5d9d7b296ddd056b
Ethan B <ethanbuss.work@gmail.com>
2019-11-21 18:26:20 -0800

    First Python drills

---
+++ python_commenting_docstrings.py
@@ -0,0 +1,19 @@
+
+
+# this is my note
+
+"""This is my note
+and this is still my note
+and this is still my note
+"""
+
+##this is a line of code
+
+
+def printMe():
+    """This is my docstring
+    """
+
+    print("Meeeeeeee")
+
+print(printMe.__doc__)
